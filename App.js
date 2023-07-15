a7491e0daa322f141fe3819fad96ae697c882c16
ruchikageeminda97 <ruchikarg97@gmail.com>
2023-07-16 01:11:52 +0530

    commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './screens/Home'
+
+const App = () => {
+  return (
+   <View className="bg-yellow-500 flex-1 items-center justify-center">
+    <Text className="font-bold">ffff</Text>
+    <Text className="font-bold">ffff</Text>
+     <Text className="font-bold">ffff</Text>
+   </View>
+  )
+}
+
+export default App
